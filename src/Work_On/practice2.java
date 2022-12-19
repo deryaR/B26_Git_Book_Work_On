d74bbdcbf1d41c2a32acbc35463f0084cee59aec
Derya Reis <deryareiss@gmail.com>
2022-12-19 16:35:40 +0000

    Created practice2

---
+++ src/Work_On/practice2.java
@@ -0,0 +1,8 @@
+package Work_On;
+
+public class practice2 {
+    public static void main(String[] args) {
+        System.out.println("Hello CYDEO");
+        System.out.println("Hello WORLD");
+    }
+}
